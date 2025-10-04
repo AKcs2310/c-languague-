6d1f569d4f28e5d33b4cc59c9e2e0331f234ad13
AKcs2310 <ankitkrgupta21@gmail.com>
2025-10-05 02:38:37 +0530

    third commit

--- chapter 2/1.c
+++ chapter 2/1.c
@@ -1,5 +1,8 @@
-#include<stdio.h>
- int main(){
-     
- }
- 
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    int j = i;
+    printf(" the value of the i and j is %d and %d respectively ", i, j);
+    return 0;
+}
---
+++ chapter 2/typechange.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ int main(){
+    int a=9;
+     int b=10;
+     float c=(float)9/10;
+     printf("the value of c is %f",c);
+
+ }
