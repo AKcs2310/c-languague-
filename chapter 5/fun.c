e63a44eed6361d4f865d94490ba6d3d0125897db
AKcs2310 <ankitkrgupta21@gmail.com>
2025-10-05 13:30:05 +0530

    6th commit

---
+++ chapter 5/fun.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int sum(int a , int b){
+     int sum=a+b;
+      return sum;
+
+ }
+ int main(){
+    int res=sum(2,3);
+     printf("the sum of a and b is %d \n",res);
+     
+     
+ }
